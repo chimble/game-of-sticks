719ecf283745c157f8e6c921b7ae53bc7aaf2c0a
Cameron Moeller <cameron.moeller@gmail.com>
2016-11-01 15:30:22 -0400

    got pvp version close to done.

--- sticks.py
+++ sticks.py
@@ -1,10 +1,10 @@
 def choose_number():
-    number = 2 #input()
     while True:
-        if number in [1, 2, 3]:
-            return number
+        number = input("Please choose 1 to 3 sticks to remove.")
+        if number in ['1', '2', '3']:
+            return int(number)
         else:
-            print("Please choose 1 to 3 sticks to remove")
+            continue
 
 def initial_sticks():
     return 20
@@ -14,10 +14,25 @@ def current_sticks(initial_sticks, chosen_numbers):
     return current_count
 
 def main():
+    print("Welcome to the Game of Sticks!")
     turn_counter = 0
     chosen_numbers = 0
     num_sticks = initial_sticks()
-    number = choose_number()
-    chosen_numbers += number
-    current_sticks(num_sticks, chosen_numbers)
+    while True:
+        if (turn_counter + 1) % 2 == 1:
+            print("It's your turn, Player 1")
+            number = choose_number()
+            chosen_numbers += number
+            stick_count = current_sticks(num_sticks, chosen_numbers)
+            turn_counter += 1
+            print("There are {} sticks left".format(stick_count))
+        elif (turn_counter + 1) % 2 == 0:
+            print("It's your turn, Player 2")
+            number = choose_number()
+            chosen_numbers += number
+            stick_count = current_sticks(num_sticks, chosen_numbers)
+            turn_counter += 1
+            print("There are {} sticks left".format(stick_count))
     return chosen_numbers
+
+main()
