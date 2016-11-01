2107327846e7dfbaa0c1ec526f4de23fbcdf2a5b
Cameron Moeller <cameron.moeller@gmail.com>
2016-11-01 14:57:05 -0400

    some tests done

--- sticks.py
+++ sticks.py
@@ -1,2 +1,23 @@
+def choose_number():
+    number = 2 #input()
+    while True:
+        if number in [1, 2, 3]:
+            return number
+        else:
+            print("Please choose 1 to 3 sticks to remove")
+
+def initial_sticks():
+    return 20
+
+def current_sticks(initial_sticks, chosen_numbers):
+    current_count = initial_sticks - chosen_numbers
+    return current_count
+
 def main():
-    pass
+    turn_counter = 0
+    chosen_numbers = 0
+    num_sticks = initial_sticks()
+    number = choose_number()
+    chosen_numbers += number
+    current_sticks(num_sticks, chosen_numbers)
+    return chosen_numbers
--- sticks_test.py
+++ sticks_test.py
@@ -1,4 +1,16 @@
 from sticks import main
+from sticks import initial_sticks
+from sticks import choose_number
+from sticks import current_sticks
 
 def test_main_exists():
-    assert main()
+    assert main
+
+def test_initial_sticks():
+    assert initial_sticks() == 20
+
+def test_choose_number():
+    assert 1 <= choose_number() <= 3
+
+def test_current_sticks():
+    assert initial_sticks() - choose_number() == 18
